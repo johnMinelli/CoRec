6e93cf5a8418306c31e9fe4d2a315aceb22e166b
Plutone11011 <borelliplutone@gmail.com>
2022-01-23 18:39:33 +0100

    special tokens and default index for vocab

--- onmt/inputters/vocab.py
+++ onmt/inputters/vocab.py
@@ -1,6 +1,12 @@
 from torchtext.vocab import vocab
 from collections import Counter, OrderedDict
 
+PAD_WORD = '<blank>'
+UNK_WORD = '<unk>'
+UNK = 0
+BOS_WORD = '<s>'
+EOS_WORD = '</s>'
+
 
 def create_vocab(datasets):
     """Creates a torchtext vocabulary of source and target
@@ -12,11 +18,17 @@ def create_vocab(datasets):
         for src_text, target_txt, _, _ in dataset:
             counter.update(src_text)
             counter.update(target_txt)
-
+    counter.update([PAD_WORD, UNK_WORD, BOS_WORD, EOS_WORD])
     sorted_by_freq_words = sorted(counter.items(), key=lambda x: x[1], reverse=True)
     ordered_dict_words = OrderedDict(sorted_by_freq_words)
 
-    return vocab(ordered_dict_words)
+    final_vocab = vocab(ordered_dict_words)
+    final_vocab.set_default_index(get_max_index(final_vocab))
+    return final_vocab
+
+
+def get_max_index(vocabulary):
+    return len(vocabulary.get_itos()) + 1
 
 
 def get_indices(vocabulary, example):
