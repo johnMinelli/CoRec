04ddc77a6c1accc84f24cb7221b1050d4ace2e3d
Simone Mattioli <simo.mattioli1998@gmail.com>
2022-02-08 18:46:48 +0100

    fix transformer trainer

--- evaluate_res.py
+++ evaluate_res.py
@@ -21,14 +21,14 @@ def main(hyp, ref):
 
 
 if __name__ == '__main__':
-    pred_CoRec = "C:/Users/Gio/PycharmProjects/CoMeatIt/data/output/1000test.out"
-    pred_ptg = "C:/Users/Gio/PycharmProjects/CoRec/result/ptg/cleaned.test.msg"
-    pred_nngen = "C:/Users/Gio/PycharmProjects/CoRec/result/nngen/cleaned.test.msg"
-    ref = "C:/Users/Gio/PycharmProjects/CoMeatIt/data/top1000/cleaned.test.msg"
+    pred_CoRec = "data/output/1000test.out"
+    #pred_ptg = "result/ptg/cleaned.test.msg"
+    #pred_nngen = "result/nngen/cleaned.test.msg"
+    ref = "data/top1000/cleaned.test.msg"
 
-    print("PTG")
-    main(pred_ptg, ref)
-    print("NNGEN")
-    main(pred_nngen, ref)
-    print("CoRec")
+    #print("PTG")
+    #main(pred_ptg, ref)
+    #print("NNGEN")
+    #main(pred_nngen, ref)
+    #print("CoRec")
     main(pred_CoRec, ref)
